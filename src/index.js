426c68309a42be1b9661af95c3c7ef4f083d3eb9
a-hodackov <hodackoff1995@gmail.com>
2021-03-13 11:51:22 +0300

    add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let stack = [];
+    let existBracket = {};
+    for (let j = 0; j < bracketsConfig.length; j++) {
+        existBracket[bracketsConfig[j][0]] = bracketsConfig[j][1];
+    }
+
+    stack.push(str.charAt(0));
+
+    for (let i = 1; i < str.length; i++) {
+        if (str.charAt(i) === existBracket[stack[stack.length - 1]]) {
+            stack.pop();
+        } else {
+            stack.push(str.charAt(i));
+        }
+    }
+    if (stack.length > 0) {
+        return false;
+    }
+    return true;
+};
